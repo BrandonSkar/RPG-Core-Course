9a6d50ec6a003fb74728f3ee64058d22f97f3f97
BrandonSkar <bransk01@hotmail.com>
2021-12-25 01:54:25 -0800

    Created Portal

---
+++ RPG Core Course/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool hasTriggered = false;
+
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(!hasTriggered && other.tag == "Player")
+            {
+                hasTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
