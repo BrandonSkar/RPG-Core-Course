3d065f8c94726318042676fc6286dda9dbbae860
BrandonSkar <bransk01@hotmail.com>
2021-12-27 00:26:36 -0800

    Items respawn in the world

--- RPG Core Course/Assets/Scripts/Combat/WeaponPickup.cs
+++ RPG Core Course/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,17 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace RPG.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5.0f;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool isShowing)
+        {
+            GetComponent<Collider>().enabled = isShowing;
+
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(isShowing);
             }
         }
     }
